1cd8bf27d89719155aa3aab049aa3fedf1e86b50
john <john@DESKTOP-CHEKDSH>
2020-10-01 20:46:58 -0400

    worked on library project

--- src/library/Book.java
+++ src/library/Book.java
@@ -5,4 +5,10 @@ package library;
 public class Book {
     private String author;
     private String title;
+    
+    public Book(String author, String title){
+        this.author = author;
+        this.title = title;
+    }
+    
 }
--- src/library/Patron.java
+++ src/library/Patron.java
@@ -3,5 +3,11 @@ package library;
 
 
 public class Patron {
+    private String name;
+    
+    public Patron(String name){
+        this.name = name;
+    }
+    
     
 }
